000db45c122d32120ccab01f81a26362ad070205
Revenger220 <maksim_korneev@ukr.net>
2015-02-02 01:24:22 +0200

    второй комент

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Новый проэкт</title>
+    </head>
+
+
+    <body>
+
+
+        <?php 
+        echo "Helli wirld!";
+        ?>
+
+
+
+
+    </body>
+</html>
